5fc9bd350fec8f10da775070f6b9dcd432766543
shayanahmad1999 <shayanahmad235@gmail.com>
2025-08-08 18:54:00 +0500

    artisan Notification are created and work done

---
+++ app/Notifications/NewMessageNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewMessageNotification extends Notification
+{
+    use Queueable;
+
+    public $message;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via($notifiable)
+    {
+        return ['database', 'mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject('New Message Notification')
+            ->line($this->message)
+            ->action('Check It Out', url('/notifications'));
+    }
+
+    public function toDatabase($notifiable)
+    {
+        return [
+            'message' => $this->message,
+            'url' => url('/notifications'),
+        ];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
